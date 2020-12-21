c535d1688430c4730225063521b0ee62a41ef6ed
RISKYSHOT <52458646+Risky-Shot@users.noreply.github.com>
2020-12-22 03:06:20 +0530

    Update aim_s.lua

--- aimlogs/aim_s.lua
+++ aimlogs/aim_s.lua
@@ -7,5 +7,18 @@ AddEventHandler('aimlogs:log', function(pedId)
     local _source = source
     local name = GetPlayerName(_source)
     local targetName = GetPlayerName(pedId)
-    PerformHttpRequest(hook, function(err, text, headers) end, 'POST', json.encode({embeds={{title="_**Aim Logs**__",description="\nPlayer name: "..name.."\nis aiming: "..targetName.."",color=16711680}}}), { ['Content-Type'] = 'application/json' })
+    table.insert(dataTable, {pName = name , aiming = targetName})
 end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(60000)
+		for k,v in pairs(dataTable) do
+            if #dataTable ~= nil then
+                PerformHttpRequest(hook, function(err, text, headers) end, 'POST', json.encode({embeds={{title="_**Aim Logs**__",description="\nPlayer name: "..v.pName.."\nis aiming: "..v.aiming.."",color=16711680}}}), { ['Content-Type'] = 'application/json' }) 
+			end
+		end
+        dataTable = {}
+	end
+end)
+
